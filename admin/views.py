884064e7a7dafd31c047ca4ccf2bf46e65c2cb2d
Faris Zahid <fariszahid02@gmail.com>
2024-05-14 03:12:16 +0100

    Add admin views for user management and activity tracking

--- admin/views.py
+++ admin/views.py
@@ -1,5 +1,24 @@
-from flask import Blueprint
-
+from flask import Blueprint, render_template, flash
+from flask_login import login_required, current_user
 
 
 admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
+
+@admin_blueprint.route('/admin')
+def admin():
+    return render_template('admin/admin.html')
+
+# view all registered users
+@admin_blueprint.route('/view_all_users', methods=['POST'])
+@login_required
+def view_all_users():
+
+    return render_template('admin/admin.html')
+
+
+
+# views all user activities
+@admin_blueprint.route('/view_user_activity', methods=['POST'])
+@login_required
+def view_user_activity():
+    return render_template('admin/admin.html')

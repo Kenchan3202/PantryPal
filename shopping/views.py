7e39217e1004aee4f328561439d85fc9be9e3570
Jacob <J.Norman2@newcastle.ac.uk>
2024-05-14 12:30:01 +0100

    Created view functions for completing shopping and adding items to shopping list

--- shopping/views.py
+++ shopping/views.py
@@ -1,9 +1,22 @@
 from flask import Blueprint, Flask, render_template, request, redirect, url_for, session, flash
 
-
 from user import user
 
 shopping_blueprint = Blueprint('shopping', __name__, template_folder='templates')
+
+
 @shopping_blueprint.route('/shopping_list')
 def shopping_list():
     return render_template('shopping/shopping_list.html')  # Adjust the template name as necessary
+
+
+@shopping_blueprint.route('/complete_shopping')
+def complete_shopping():
+    print(f"Complete shopping")
+    return render_template('shopping/shopping_list.html')
+
+
+@shopping_blueprint.route('/add_item')
+def add_item():
+    print(f"Add items")
+    return render_template('shopping/shopping_list.html')

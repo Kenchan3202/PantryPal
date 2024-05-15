c0f2b75a355468540368170efb59f680c964f014
Faris Zahid <fariszahid02@gmail.com>
2024-05-16 00:54:20 +0100

    Added view functions for recipe/views.py

--- recipes/views.py
+++ recipes/views.py
@@ -10,17 +10,27 @@ print("Template folder:", recipes_blueprint.template_folder)
 
 @recipes_blueprint.route('/recipes')
 @login_required
-def recipe_detail():
+def recipe_list():
     return render_template('recipes/recipes.html')  # Adjust the template name as necessary
 
 
 @recipes_blueprint.route('/add_recipes')
 @login_required
-def add_detail():
+def add_recipe():
     return render_template('recipes/add_recipes.html')
 
 
 @recipes_blueprint.route('/edit_recipes')
 @login_required
-def edit_detail():
+def edit_recipe():
+    return render_template('recipes/edit_recipes.html')
+
+@recipes_blueprint.route('/delete_recipe', methods=['POST'])
+@login_required
+def delete_recipe():
+    return render_template('recipes/edit_recipes.html')
+
+@recipes_blueprint.route('/rate_recipe', methods=['POST'])
+@login_required
+def rate_recipe():
     return render_template('recipes/edit_recipes.html')

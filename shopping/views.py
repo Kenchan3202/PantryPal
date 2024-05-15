9de39d9b5ff8ef64b25bd5e97367c71020f210c0
Jacob <J.Norman2@newcastle.ac.uk>
2024-05-15 12:15:36 +0100

    Added input form for adding a new item and basic functionality for it in views.py

--- shopping/views.py
+++ shopping/views.py
@@ -1,22 +1,31 @@
 from flask import Blueprint, Flask, render_template, request, redirect, url_for, session, flash
-
+from models import FoodItem
+from app import db
 from user import user
+from shopping.forms import AddItemForm
 
 shopping_blueprint = Blueprint('shopping', __name__, template_folder='templates')
 
 
-@shopping_blueprint.route('/shopping_list')
+@shopping_blueprint.route('/shopping_list', methods=['GET', 'POST'])
 def shopping_list():
-    return render_template('shopping/shopping_list.html')  # Adjust the template name as necessary
+    form = AddItemForm()
+
+
+
+    return render_template('shopping/shopping_list.html', form = form)  # Adjust the template name as necessary
 
 
 @shopping_blueprint.route('/complete_shopping')
 def complete_shopping():
+    # Move all items in shopping list field to pantry field then clear shopping list
     print(f"Complete shopping")
     return render_template('shopping/shopping_list.html')
 
 
 @shopping_blueprint.route('/add_item')
 def add_item():
+    # query food item entered, if it exists, add it to shopping list field.
+    # If it doesn't exist, alert user and give option to add it to DB
     print(f"Add items")
     return render_template('shopping/shopping_list.html')

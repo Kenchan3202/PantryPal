a88f492438d17adf1282991644cef2fbb372aea5
Jacob <J.Norman2@newcastle.ac.uk>
2024-05-16 10:49:19 +0100

    Minor changes to views and shopping.html

--- shopping/views.py
+++ shopping/views.py
@@ -1,18 +1,15 @@
 from flask import Blueprint, Flask, render_template, request, redirect, url_for, session, flash
-from models import FoodItem
-from app import db
+from flask_login import current_user
 from shopping.forms import AddItemForm
+
 
 shopping_blueprint = Blueprint('shopping', __name__, template_folder='templates')
 
 
 @shopping_blueprint.route('/shopping_list', methods=['GET', 'POST'])
 def shopping_list():
-    form = AddItemForm()
 
-
-
-    return render_template('shopping/shopping_list.html', form = form)  # Adjust the template name as necessary
+    return render_template('shopping/shopping_list.html')  # Adjust the template name as necessary
 
 
 @shopping_blueprint.route('/complete_shopping', methods=['GET'])
@@ -22,9 +19,9 @@ def complete_shopping():
     return render_template('shopping/shopping_list.html')
 
 
-@shopping_blueprint.route('/add_item')
+@shopping_blueprint.route('/add_item', methods = ['GET','POST'])
 def add_item():
     # query food item entered, if it exists, add it to shopping list field.
     # If it doesn't exist, alert user and give option to add it to DB
-    print(f"Add items")
-    return render_template('shopping/shopping_list.html')
+    form = AddItemForm()
+    return render_template('shopping/shopping_list.html', form = form)

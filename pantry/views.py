715a84e1d12478cdbc8274bfafd3ce3449aa4997
kenchan6666 <168535414+kenchan6666@users.noreply.github.com>
2024-05-17 20:53:44 +0100

    updated search food page and function

--- pantry/views.py
+++ pantry/views.py
@@ -76,66 +76,21 @@ def create_item():
     return render_template('pantry/add_food.html')
 
 
-# @pantry_blueprint.route('/create_item', methods=['GET', 'POST'])
-# @login_required
-# def create_item():
-#     if request.method == 'POST':
-#         item_name = request.form['name']
-#         expiry_date = request.form['expiry_date']
-#         quantity = request.form['quantity']
-#         units = request.form['units']
-#         calories = request.form['calories']
-#         description = request.form['description']
-#
-#         # 查找是否已有此食物
-#         food_item = FoodItem.query.filter_by(name=item_name).first()
-#         if not food_item:
-#             # 如果没有，创建新的 FoodItem
-#             food_item = FoodItem(food_name=item_name, food_description=description)
-#             db.session.add(food_item)
-#             db.session.commit()
-#
-#         # 创建或获取 QuantifiedFoodItem
-#         quantified_food_item = QuantifiedFoodItem.query.filter_by(food_id=food_item.id).first()
-#         if not quantified_food_item:
-#             quantified_food_item = QuantifiedFoodItem(food_id=food_item.id, quantity=quantity, units=units)
-#             db.session.add(quantified_food_item)
-#             db.session.commit()
-#
-#         # 创建 PantryItem
-#         new_item = PantryItem(user_id=current_user.id, qfood_id=quantified_food_item.id, expiry=expiry_date,
-#                               calories=calories)
-#         db.session.add(new_item)
-#         db.session.commit()
-#
-#         flash('Item added successfully!', 'success')
-#         return redirect(url_for('pantry.items_view'))
-#
-#     return render_template('pantry/add_food.html')
-
-
 @pantry_blueprint.route('/search', methods=['GET', 'POST'])
 @login_required
 def search():
-    item_temp = ""  # Initialize the variable to hold the result
-    item_info = ""
+    items = []  # Initialize the list to hold the result
     if request.method == 'POST':
         item_name = request.form['itemname'].strip()  # Get the item name from form input
-        pantry_item = (db.session.query(PantryItem)
-                       .join(QuantifiedFoodItem)
-                       .join(FoodItem)
-                       .filter(FoodItem.name.ilike(f"%{item_name}%"))
-                       .filter(PantryItem.user_id == current_user.id)
-                       .first())
+        items = (db.session.query(PantryItem)
+                 .join(QuantifiedFoodItem)
+                 .join(FoodItem)
+                 .filter(FoodItem.name.ilike(f"%{item_name}%"))
+                 .filter(PantryItem.user_id == current_user.id)
+                 .all())
 
-        if pantry_item:
-            item_temp = f"Item name: {pantry_item.get_name()} | Expiry date: {pantry_item.get_expiry()}"
-            item_info = f"Description: {pantry_item.qfooditem.fooditem.get_description()} | Quantity: {pantry_item.get_quantity()} {pantry_item.get_units()} | Calories: {pantry_item.get_calories()}"
-        else:
-            item_temp = "Item not found"  # Set item_temp to a not found message if no match is found
+    return render_template('pantry/search.html', items=items)  # Pass the result directly to the template
 
-    return render_template('pantry/search.html', itemtemp=item_temp,
-                           iteminfo=item_info)  # Pass the result directly to the template
 
 
 @pantry_blueprint.route('/delete_item/<int:item_id>', methods=['POST'])

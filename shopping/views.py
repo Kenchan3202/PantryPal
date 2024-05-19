a16a76c8055f0ef336e69fe898bf6d1afa230e54
Faris Zahid <fariszahid02@gmail.com>
2024-05-19 16:20:34 +0100

    Update function in shopping/views.py

--- shopping/views.py
+++ shopping/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, Flask, render_template, request, redirect, url_for, session, flash
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 import models
 from models import *
@@ -10,36 +10,36 @@ from app import db
 
 
 @shopping_blueprint.route('/shopping_list', methods=['GET', 'POST'])
+@login_required
 def shopping_list():
     form = AddItemForm()
-
-
     if form.validate_on_submit():
         food_item = form.newItem.data
         quantity = form.itemQuantity.data
-        units = form.newItem.data
+        units = form.itemUnits.data
         list_name = form.listName.data
 
-        food_id = FoodItem.query.filter_by(name=food_item)
-        if not food_id():
-            flash("Food item not found in DB")
+        food_id = FoodItem.query.filter_by(name=food_item).first()
+        if not food_id:
+            flash("Food item not found in DB", "error")
             return render_template('shopping/shopping_list.html', form=form)
         else:
-            newQFI = QuantifiedFoodItem()
-            newQFI.food_id = food_id
-            newQFI.quantity = quantity
-            newQFI.units = units
-            q_food_item = models.QuantifiedFoodItem(food_id=newQFI.food_id,
-                                                    quantity=newQFI.quantity,
-                                                    units=newQFI.units)
-            db.session.add(q_food_item)
+            newQFI = QuantifiedFoodItem(food_id=food_id.id, quantity=quantity, units=units)
+            db.session.add(newQFI)
             db.session.commit()
 
-            list_id = ShoppingList.query.filter_by(list_name=list_name)
-            new_shopping_item = models.ShoppingItem(list_id=list_id,
-                                                    qfood_id=q_food_item.id)
+            list_id = ShoppingList.query.filter_by(list_name=list_name).first()
+            if not list_id:
+                list_id = ShoppingList(user_id=current_user.id, list_name=list_name)
+                db.session.add(list_id)
+                db.session.flush()
+
+            new_shopping_item = ShoppingItem(list_id=list_id.id, qfood_id=newQFI.id)
             db.session.add(new_shopping_item)
             db.session.commit()
+            flash("Item added successfully", "success")
+
+    return render_template('shopping/shopping_list.html', form=form)
 
 
 
@@ -53,6 +53,7 @@ def shopping_list():
 
 
 @shopping_blueprint.route('/complete_shopping', methods=['GET'])
+@login_required
 def complete_shopping():
     # Move all items in shopping list field to pantry field then clear shopping list
     print(f"Complete shopping")
